af6bae1df3b000802005cdef9164b0855b6235cc
Seth Brasile <seth.brasile@gmail.com>
2016-04-14 19:08:27 -0500

    some inline docs

--- addon/utils/note.js
+++ addon/utils/note.js
@@ -6,6 +6,15 @@ const {
   computed
 } = Ember;
 
+/**
+ * Ember.Object that acts as "note".
+ * Set it's letter, octave, and (optionally) it's accidental.
+ *
+ * Computed Properties (examples for notes "Db1" and "D0" respectively):
+ *
+ * get {string} string    examples: "Db1", "D0"
+ * get {string} noteName  examples: "Db", "D"
+ */ 
 export const Note = Ember.Object.extend({
   letter: null,
   accidental: null,
@@ -45,6 +54,23 @@ export const Note = Ember.Object.extend({
   })
 });
 
+
+/**
+ * octaveShift - Takes an array of arrays of notes, determines the last note of
+ * the first array, then splits the rest of the arrays in the array at the last
+ * note of the first array, and moves the beginning of the array to the end
+ * so that each array starts at the next note after the last note of the first
+ * array, instead of at "A" (alphabetically)
+ *
+ * This is hard to explain. Here's an example.
+ * (Simplified, as the real notes are objects.)
+ *
+ * Example input: [['A0', 'B0'], ['A1', 'B1', 'C1', 'D1']]
+ * Example output: [['A0', 'B0'], ['C1', 'D1', 'A1', 'B1']]
+ *
+ * @param  {array} octaves  an array of octaves, each octave is an array of notes, each note is an Ember.Object
+ * @return {array}          same array but shifted around
+ */
 export function octaveShift(octaves) {
   // Pull first octave from beginning of array
   const firstOctave = A(A(octaves).shiftObject());
@@ -61,11 +87,21 @@ export function octaveShift(octaves) {
   .unshiftObjects([firstOctave]);
 }
 
+/**
+ * octaveSort - Maps through an array of arrays and sorts each array with
+ * "noteSort"
+ *
+ * @param  {array} octaves array of arrays to be sorted
+ * @return {array}         array of sorted arrays
+ */
 export function octaveSort(octaves) {
   return octaves.map((octave) => octave.sort(noteSort));
 }
 
-// Sorts alphabetically, flats before naturals
+/**
+ * noteSort - Acts as a comparator function for javascript's array sort() method.
+ * Sorts two Ember.Object "notes" alphabetically, flats before naturals.
+ */
 export function noteSort(a, b) {
   const aLet = a.get('letter');
   const bLet = b.get('letter');
