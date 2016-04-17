c1650b4364a01d072e842ff8719b62c1e893255b
Seth Brasile <seth.brasile@gmail.com>
2016-04-17 14:28:45 -0500

    A bunch of tests

---
+++ tests/helpers/context-mock.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+const ContextMock = Ember.Object.extend({
+  createBufferSourceCalled: false,
+  connectCalled: false,
+  startCalled: false,
+  bufferSource: null,
+  destination: Ember.computed.reads('elementId'),
+
+  createBufferSource() {
+    this.set('createBufferSourceCalled', true);
+    this.set('bufferSource', ContextMock.create());
+    return this.get('bufferSource');
+  },
+  connect(object) {
+    this.set('connectCalled', true);
+    this.set('connectedObject', object);
+  },
+  start() {
+    this.set('startCalled', true);
+  },
+  createStereoPanner() {
+    this.set('panner', { pan: { value: 0 } });
+    return this.get('panner');
+  }
+});
+
+export default ContextMock;
