7c646474c3d95f11cda58abb462dfab1acbccdf5
Seth Brasile <seth.brasile@gmail.com>
2016-04-18 03:18:33 -0500

    audio files examples

--- tests/dummy/app/controllers/audio-files.js
+++ tests/dummy/app/controllers/audio-files.js
@@ -5,20 +5,30 @@ export default Ember.Controller.extend({
 
   initAudioFiles: Ember.on('init', function() {
     const audio = this.get('audio');
-    audio.load('single-note-left', 'Eb5.mp3');
-    audio.load('single-note-right', 'Db5.mp3');
-    audio.pan('single-note-left',  -1);
-    audio.pan('single-note-right',  1);
+
+    // Db5.mp3 is an mp3 file located in this project's "public" folder
+    audio.load('single-note-left', 'Db5.mp3');
+
+    // This one is loaded from a URL somewhere on the internet
+    audio.load('single-note-right', 'https://raw.githubusercontent.com/mudcube/MIDI.js/master/examples/soundfont/acoustic_grand_piano-mp3/B5.mp3');
+
+    // You can pan a note left (any value between -1 and -0.1)
+    audio.pan('single-note-left',  -0.7);
+
+    // You can pan a note right (any value between 0.1 and 1)
+    audio.pan('single-note-right',  0.7);
   }),
 
   actions: {
     playSingleNoteLeft() {
       this.get('audio').play('single-note-left');
     },
+
     playSingleNoteRight() {
       this.get('audio').play('single-note-right');
     },
-    playTwoPannedNotes() {
+
+    playBothNotes() {
       const audio = this.get('audio');
       audio.play('single-note-left');
       audio.play('single-note-right');
