3aa90a9e4c7aba8719e592d87e14ca0ed3009510
Seth Brasile <seth.brasile@gmail.com>
2016-06-26 16:08:36 -0500

    use the Track instance directly instead of proxying everything through the audio service

--- tests/dummy/app/controllers/audio-files/mp3-player.js
+++ tests/dummy/app/controllers/audio-files/mp3-player.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
-import getProp from 'ember-audio/utils/get-prop';
 
 const {
+  computed,
+  computed: { reads },
   inject: { service },
   Controller
 } = Ember;
@@ -10,30 +11,6 @@ export default Controller.extend({
   audio: service(),
   selectedTrack: null,
   isPlaying: false,
-
-  duration: getProp('duration').fromTrack('selectedTrack.name'),
-  position: getProp('position').fromTrack('selectedTrack.name'),
-
-  percentPlayed: getProp('percentPlayed')
-    .fromTrack('selectedTrack.name', function(percentPlayed) {
-      return Ember.String.htmlSafe(`width: ${percentPlayed}%;`);
-    }
-  ),
-
-  // Doesn't work
-  percentPlayed2: getProp('percentPlayed')
-    .fromTrack('selectedTrack.name', function(percentPlayed) {
-      // percentPlayed is undefined
-      return Ember.String.htmlSafe(`width: ${percentPlayed}%;`);
-    }
-  ),
-
-  percentGain: getProp('percentGain')
-    .fromTrack('selectedTrack.name', function(percentGain) {
-      return Ember.String.htmlSafe(`height: ${percentGain}%;`);
-    }
-  ),
-
   tracks: [
     {
       name: 'barely-there',
@@ -54,51 +31,54 @@ export default Controller.extend({
     }
   ],
 
+  percentPlayed: computed('selectedTrack.percentPlayed', function() {
+    const percentPlayed = this.get('selectedTrack.percentPlayed');
+    return Ember.String.htmlSafe(`width: ${percentPlayed}%;`);
+  }),
+
+  percentGain: computed('selectedTrack.percentGain', function() {
+    const percentGain = this.get('selectedTrack.percentGain');
+    return Ember.String.htmlSafe(`width: ${percentGain}%;`);
+  }),
+
   actions: {
-    selectTrack(track) {
+    selectTrack(newTrack) {
       const audio = this.get('audio');
+
+      audio.load(`${newTrack.name}.mp3`).asTrack(newTrack.name)
+        .then((track) => {
+          track.set('details', newTrack);
+          this.set('selectedTrack', track);
+        });
 
       this.set('isPlaying', false);
       audio.pauseAll();
-
-      track.promise = audio.load(`${track.name}.mp3`).asTrack(track.name);
-      this.set('selectedTrack', track);
     },
 
     play() {
-      this.get('selectedTrack.promise').then((track) => {
-        this.set('isPlaying', true);
-        track.play();
-      });
+      this.get('selectedTrack').play();
+      this.set('isPlaying', true);
     },
 
     pause() {
-      const trackName = this.get('selectedTrack.name');
+      this.get('selectedTrack').pause()
       this.set('isPlaying', false);
-      this.get('audio').getTrack(trackName).pause();
     },
 
     seek(e) {
-      const audio = this.get('audio');
-      const trackName = this.get('selectedTrack.name');
-
       const width = e.target.offsetParent.offsetWidth;
       const newPosition = e.offsetX / width;
-
-      audio.getTrack(trackName).seek(newPosition).from('ratio');
+      this.get('selectedTrack').seek(newPosition).from('ratio');
     },
 
     changeVolume(e) {
-      const audio = this.get('audio');
-      const trackName = this.get('selectedTrack.name');
-
       const height = e.target.offsetParent.offsetHeight;
       const offset = e.pageY - Ember.$(e.target).parent().offset().top;
       const adjustedHeight = height * 0.8;
       const adjustedOffset = offset - ((height - adjustedHeight) / 2);
       const newGain = adjustedOffset / adjustedHeight;
 
-      audio.getTrack(trackName).changeGain(newGain).from('inverseRatio');
+      this.get('selectedTrack').changeGain(newGain).from('inverseRatio');
     }
   }
 });
