b14c534731bf71f089dd3c82f7835bd320e46026
Seth Brasile <seth.brasile@gmail.com>
2016-07-07 16:36:20 -0500

    better loading indicator on drum kit

--- tests/dummy/app/controllers/audio-files/drum-kit.js
+++ tests/dummy/app/controllers/audio-files/drum-kit.js
@@ -3,16 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   audio: Ember.inject.service(),
   beats: Ember.A(),
+  isLoading: true,
 
   loadSound(name) {
-    this.get('audio').load(`/ember-audio/${name}.wav`).asSound(name)
+    return this.get('audio').load(`/ember-audio/${name}.wav`).asSound(name)
       .then((sound) => this.get('beats').pushObject({ name, sound }));
   },
 
   initBeats: Ember.on('init', function() {
-    this.loadSound('kick');
-    this.loadSound('snare');
-    this.loadSound('hihat');
+    Ember.RSVP.all([
+      this.loadSound('kick'),
+      this.loadSound('snare'),
+      this.loadSound('hihat')
+    ])
+    .then(() => this.set('isLoading', false));
   }),
 
   actions: {
