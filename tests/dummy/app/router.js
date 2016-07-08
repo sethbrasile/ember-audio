37afaa06ce2a44c0f0f149b95ecb3d2c3a5ae63d
Seth Brasile <seth.brasile@gmail.com>
2016-07-08 17:34:20 -0500

    put sound font code on same page as sound font example

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -8,8 +8,6 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('soundfonts', function() {
-    this.route('play', { path: '/' });
-    this.route('code');
     this.route('notes');
   });
   this.route('audio-files', function() {
