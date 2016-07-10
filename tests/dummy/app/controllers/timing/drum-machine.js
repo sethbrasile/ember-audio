30853804d18337b860593009a504c5548d0fda8d
Seth Brasile <seth.brasile@gmail.com>
2016-07-10 13:01:45 -0500

    adjusted docs to match changes

--- tests/dummy/app/controllers/timing/drum-machine.js
+++ tests/dummy/app/controllers/timing/drum-machine.js
@@ -2,39 +2,39 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   audio: Ember.inject.service(),
-  drums: null,
+  beatTracks: null,
   isLoading: true,
   bpm: 120,
 
-  loadDrum(name) {
-    return this.get('audio').load(`/ember-audio/${name}.wav`).asBeatTrack(name);
-  },
-
   initBeats: Ember.on('init', function() {
     Ember.RSVP.all([
-      this.loadDrum('kick'),
-      this.loadDrum('snare'),
-      this.loadDrum('hihat')
+      this._loadBeatTrackFor('kick'),
+      this._loadBeatTrackFor('snare'),
+      this._loadBeatTrackFor('hihat')
     ])
-    .then((drums) => {
+    .then((beatTracks) => {
       // default is 4 beats, but we're going to use 8
-      drums.map((drum) => drum.set('numBeats', 8));
+      beatTracks.map((beatTrack) => beatTrack.set('numBeats', 8));
       this.set('isLoading', false);
-      this.set('drums', drums);
+      this.set('beatTracks', beatTracks);
     });
   }),
 
+  _loadBeatTrackFor(name) {
+    return this.get('audio').load(`/ember-audio/${name}.wav`).asBeatTrack(name);
+  },
+
   actions: {
     play() {
-      this.get('drums').map((drum) => {
+      this.get('beatTracks').map((beatTrack) => {
         // playActiveBeats() optionally accepts "noteType" which defaults to "1/4"
         // notes, but we want to use eighth notes
-        drum.playActiveBeats(this.get('bpm'), 1/8);
+        beatTrack.playActiveBeats(this.get('bpm'), 1/8);
 
         // /* playActiveBeats() is a convenience method. For more control, you could do:
         // http://bradthemad.org/guitar/tempo_explanation.php */
         // const eighthNoteDuration = (240 * 1/8) / this.get('bpm');
-        // drum.get('beats').map((beat, beatIndex) => {
+        // beatTrack.get('beats').map((beat, beatIndex) => {
         //   /* whatever else you need to do */
         //   beat.ifActivePlayIn(beatIndex * eighthNoteDuration);
         // });
