fd0871547cd6f85cebe65eda1ab67fae13f81b0d
Seth Brasile <seth.brasile@gmail.com>
2016-07-23 10:55:29 -0500

    period

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -11,7 +11,7 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 if (!window.AudioContext) {
   document.write(`Oh poo. Looks like this browser doesn't support the Web Audio API.<br><br>`);
-  document.write('<a href="http://caniuse.com/#feat=audio-api">See supported browsers</a><br><br>');
+  document.write('<a href="http://caniuse.com/#feat=audio-api">See supported browsers.</a><br><br>');
   document.write('<a href="http://lmgtfy.com/?q=web+audio+api+audiocontext+polyfill">There are polyfills, but I have not tested them.</a>');
 }
 
