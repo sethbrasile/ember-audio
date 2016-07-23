e435fb37c0861777b5fcc2b1fc3e6b0bae1d2fa5
Seth Brasile <seth.brasile@gmail.com>
2016-07-23 10:54:06 -0500

    Fixed dummy app to work in safari. Also a message in dummy app for unsupported browsers.

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -7,6 +7,14 @@ const {
   Application
 } = Ember;
 
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+if (!window.AudioContext) {
+  document.write(`Oh poo. Looks like this browser doesn't support the Web Audio API.<br><br>`);
+  document.write('<a href="http://caniuse.com/#feat=audio-api">See supported browsers</a><br><br>');
+  document.write('<a href="http://lmgtfy.com/?q=web+audio+api+audiocontext+polyfill">There are polyfills, but I have not tested them.</a>');
+}
+
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
