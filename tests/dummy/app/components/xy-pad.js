51fe7a0157ae810a3354ac251eed5c3d979fb500
Seth Brasile <seth.brasile@gmail.com>
2016-07-27 13:17:10 -0500

    grid and text for xy pad

--- tests/dummy/app/components/xy-pad.js
+++ tests/dummy/app/components/xy-pad.js
@@ -2,11 +2,72 @@ import Ember from 'ember';
 import layout from '../templates/components/xy-pad';
 
 const {
+  run: { schedule },
   Component
 } = Ember;
 
 export default Component.extend({
   layout,
+
+  didInsertElement() {
+    schedule('afterRender', this, '_drawGrid');
+    schedule('afterRender', this, '_drawText');
+  },
+
+  _drawText() {
+    const [ canvas ] = this.$('canvas');
+    const ctx = canvas.getContext('2d');
+    const pad = 10;
+
+    ctx.fillStyle = 'white';
+    ctx.font = "24px serif";
+    ctx.textAlign = "left";
+
+    // save orientation
+    ctx.save();
+
+    // rotate canvas
+    ctx.translate(0, 0);
+    ctx.rotate( Math.PI / 2 );
+
+    // draw 'Gain'
+    ctx.fillText('Gain', pad, - pad);
+
+    // restore orientation
+    ctx.restore();
+
+    // draw "Frequency"
+    ctx.fillText('Frequency', pad, canvas.height - pad);
+  },
+
+  _drawGrid() {
+    const [ canvas ] = this.$('canvas');
+    const ctx = canvas.getContext('2d');
+    const width = canvas.width;
+    const height = canvas.height;
+    const gridSize = 30;
+
+    ctx.strokeStyle = 'gray';
+    ctx.strokeWidth = 1;
+
+    ctx.beginPath();
+
+    for (let i = 1; i <= width / gridSize; i++) {
+      const x = (i * gridSize);
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, height);
+      ctx.stroke();
+    }
+
+    for (let i = 1; i <= height / gridSize; i++) {
+      const y = (i * gridSize);
+      ctx.moveTo(0, y);
+      ctx.lineTo(width, y);
+      ctx.stroke();
+    }
+
+    ctx.closePath();
+  },
 
   actions: {
     activate() {
