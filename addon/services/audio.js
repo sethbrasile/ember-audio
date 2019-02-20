2946f361815b6820746db0cf335d1102b42428fe
Seth Brasile <seth.brasile@gmail.com>
2019-02-19 20:29:17 -0600

    polyfill AudioContext

--- addon/services/audio.js
+++ addon/services/audio.js
@@ -1,4 +1,4 @@
-import { resolve, all } from 'rsvp';
+import { resolve, all, Promise } from 'rsvp';
 import Service from '@ember/service';
 import fetch from 'fetch';
 import {
@@ -17,6 +17,18 @@ import {
   mungeSoundFont,
   frequencyMap
 } from 'ember-audio/utils';
+
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+// Polyfill AudioContext for Safari
+// https://gist.github.com/jakearchibald/131d7101b134b6f7bed1d8320e4da599
+if (!window.AudioContext && window.webkitAudioContext) {
+  const oldFunction = AudioContext.prototype.decodeAudioData;
+
+  AudioContext.prototype.decodeAudioData = function(arraybuffer) {
+    return new Promise((resolve, reject) => oldFunction.call(this, arraybuffer, resolve, reject));
+  }
+}
 
 /**
  * Provides the Audio Service
