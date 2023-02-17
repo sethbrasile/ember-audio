ff6c376bec2b5894b8ca4e985c49468098bc085a
Seth Brasile <seth.brasile@gmail.com>
2023-02-17 15:16:34 -0600

    mock web audio api

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,6 +4,9 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import wamock from 'web-audio-mock-api';
+
+window.AudioContext = wamock.AudioContext;
 
 setApplication(Application.create(config.APP));
 
