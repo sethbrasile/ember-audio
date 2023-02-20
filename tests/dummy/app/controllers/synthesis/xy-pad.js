967a83b8e1101296c454e690bd19a8aa089ff224
Seth Brasile <seth.brasile@gmail.com>
2023-02-19 22:00:10 -0600

    js class controller

--- tests/dummy/app/controllers/synthesis/xy-pad.js
+++ tests/dummy/app/controllers/synthesis/xy-pad.js
@@ -1,32 +1,28 @@
-import classic from 'ember-classic-decorator';
-import { action, computed } from '@ember/object';
-import { on } from '@ember-decorators/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 import { exponentialRatio } from 'ember-audio/utils';
 
-@classic
 export default class XyPadController extends Controller {
-  @service
-  audio;
+  @service audio;
+  @tracked oscillator;
+  @tracked range;
 
-  oscillator = null;
   padSize = 300;
-  range = null;
 
-  @on('init')
-  initOscillator() {
-    const oscillator = this.audio.createOscillator({ type: 'square' });
-    this.set('oscillator', oscillator);
+  constructor() {
+    super(...arguments);
+
+    this.oscillator = this.audio.createOscillator({ type: 'square' });
 
     if (!this.range) {
       this.range = { min: 100, max: 400 };
     }
   }
 
-  @computed('oscillator.frequency')
   get frequency() {
-    const frequency = this.get('oscillator.frequency');
+    const frequency = this.oscillator.frequency;
 
     if (frequency) {
       return frequency.toFixed();
@@ -35,28 +31,14 @@ export default class XyPadController extends Controller {
     return null;
   }
 
-  @computed('oscillator.gain')
   get gain() {
-    const gain = this.get('oscillator.gain');
+    const gain = this.oscillator.gain;
 
     if (gain) {
       return gain.toFixed(2);
     } else {
       return 0;
     }
-  }
-
-  _getFrequency(x) {
-    const range = this.range;
-    const padSize = this.padSize;
-
-    return range.min + (range.max - range.min) * (x / padSize);
-  }
-
-  _getGain(y) {
-    // Human senses are not linear.
-    // http://stackoverflow.com/questions/1165026/what-algorithms-could-i-use-for-audio-volume-level
-    return exponentialRatio(y / this.padSize);
   }
 
   @action
@@ -66,20 +48,26 @@ export default class XyPadController extends Controller {
 
   @action
   stop() {
-    const oscillator = this.oscillator;
+    const { oscillator } = this;
 
-    if (oscillator.get('isPlaying')) {
+    if (oscillator.isPlaying) {
       oscillator.stop();
     }
   }
 
   @action
   adjustSynthParams(x, y) {
-    const oscillator = this.oscillator;
-    const frequency = this._getFrequency(x);
-    const gain = this._getGain(y);
+    const { oscillator, range, padSize } = this;
+    const frequency = range.min + (range.max - range.min) * (x / padSize);
+
+    // Human senses are not linear.
+    // http://stackoverflow.com/questions/1165026/what-algorithms-could-i-use-for-audio-volume-level
+    const gain = exponentialRatio(y / this.padSize);
 
     oscillator.update('frequency', frequency);
     oscillator.update('gain', gain);
+
+    // Must reassign in order for @tracked to pick up changes to complex object
+    this.oscillator = oscillator;
   }
 }
